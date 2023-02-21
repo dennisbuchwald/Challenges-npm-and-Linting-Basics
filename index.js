15133a296ef92fcd78e2a401a49b30f1aa0f816c
Dennis Buchwald <buchwald95@icloud.com>
2023-02-21 11:44:51 +0100

    fixed code with help from npm

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function toggleButton() {
   const cards = document.querySelectorAll('[data-js=card]');
+  
   cards.forEach(card => {
     const button = card.querySelector('[data-js="button"]');
     const link = card.querySelector('[data-js="link"]');
     const arrow = card.querySelector('[data-js="arrow"]');
     button.addEventListener('click', () => {
-      link.classList.toggle('hide');
+      link.classList.toggle('hidden');
       arrow.classList.toggle('rotate');
     });
   });
